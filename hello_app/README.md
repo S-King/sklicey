0823166c48f452b1e5a006dd145fa1b54da31881
Sklicey <sking@synensishealth.com>
2015-05-20 03:24:32 +0000

    Badass readme shit

---
+++ hello_app/README.md
@@ -0,0 +1,7 @@
+== README
+
+This is my new readme fa sho.
+
+
+Please feel free to use a different markup language if you do not plan to run
+<tt>rake doc:app</tt>.
--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root". The syntax of this is controller#method (term?)
-   root 'application#hello'
+   root 'application#Introduction'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
